bbd16b8480ef31fafd8179c99326560ca9d3c63c
nzreal <296488372@qq.com>
2022-08-26 13:06:03 +0800

    chore: challenge 459

--- questions/00459-medium-flatten/template.ts
+++ questions/00459-medium-flatten/template.ts
@@ -1,1 +1,19 @@
-type Flatten = any
+type Flatten<ARR extends any[]> =
+  ARR extends [infer C, ... infer REST] ?
+    C extends any[] ? [...Flatten<C>, ...Flatten<REST>]: [C, ...Flatten<REST>]: ARR
+
+type test459 = Flatten<[1, 2, [3]]>
+
+function flatten<T extends any[]>(arr: T): Flatten<T> {
+  const res: any[] = []
+
+  arr.forEach((item) => {
+    if (Array.isArray(item))
+      res.push(...flatten(item))
+
+    else
+      res.push(item)
+  })
+
+  return res as Flatten<T>
+}
