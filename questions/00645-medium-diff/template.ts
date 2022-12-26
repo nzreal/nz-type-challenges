02d2e0fafc3687208164c2a2ce72b0856761a949
nzreal <296488372@qq.com>
2022-12-26 23:48:09 +0800

    chore: prac 00645 diff

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,5 @@
-type Diff<O, O1> = any
+type GetDiffedKeys<O, O1> = { [K in keyof O]: K extends keyof O1? never: K }[keyof O] | { [K in keyof O1]: K extends keyof O? never: K }[keyof O1]
+
+type Diff<O, O1> = GetDiffedKeys<O, O1> extends (keyof O | keyof O1) ? Pick<O&O1, GetDiffedKeys<O, O1>> :never
+
+type test645 = Diff<{ a: number; b: number }, { b: number; c: number }>
