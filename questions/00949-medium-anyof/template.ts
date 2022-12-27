212a01d2bef16bb6fdf42dba8c5bc58f62f37d00
nzreal <296488372@qq.com>
2022-12-27 22:53:16 +0800

    chore: challenge 949

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,10 @@
-type AnyOf<T extends readonly any[]> = any
+type FalsyArray = [false, 0, '', [], {}]
+
+// TODO: 为什么调换 C、T 就不行了
+type isTruthy<T, F = FalsyArray> = F extends [infer C, ...infer R] ?
+  C extends T ? false: isTruthy<T, R>: true
+
+type AnyOf<T extends readonly any[]> = T extends [infer C, ...infer R]?
+  isTruthy<C> extends true? true: AnyOf<R> : false
+
+type test_949 = isTruthy<1>
