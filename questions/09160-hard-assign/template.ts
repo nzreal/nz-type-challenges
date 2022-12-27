656f69255c8349c8aaae75ac7eee39850e85eaa3
nzreal <296488372@qq.com>
2022-12-27 16:39:02 +0800

    chore: challenge 9160

--- questions/09160-hard-assign/template.ts
+++ questions/09160-hard-assign/template.ts
@@ -1,1 +1,8 @@
-type Assign<T extends Record<string, unknown>, U> = any
+type MergeObject<O extends Record<string, unknown>, O1 extends Record<string, unknown>> =
+  { [K in (keyof O | keyof O1)]: K extends keyof O1 ? O1[K]: K extends keyof O ? O[K]: never }
+
+type Assign<T extends Record<string, unknown>, U extends any[], Result extends Record<string, any> = T> =
+U extends [infer R, ...infer J]
+  ? R extends Record<string, unknown> ? Assign<T, J, MergeObject<Result, R>>: Assign<T, J, Result>
+  :
+  Result
