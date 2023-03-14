0f8dc38227e45f8f490c6add0d9fa3dd57281c35
nzreal <296488372@qq.com>
2023-03-14 19:45:57 +0800

    chore: 01097, 01130

--- questions/01097-medium-isunion/template.ts
+++ questions/01097-medium-isunion/template.ts
@@ -1,1 +1,18 @@
-type IsUnion<T> = any
+type isNever<T> = [T] extends [never] ? true : false
+
+// TODO: never extends never = never ?
+
+/**
+ * 首先判断是否 never，为 never 时会直接 return never
+ *
+ * 通过 I extends I 进行联合类型分配，分别是 'a' extends 'a' / 'b' extends 'b' / 'c' extends 'c'
+ *
+ * 而联合类型在条件判断的右侧出现则不会有这个机制，于是 [T] extends [I] 就变成了 ['a' | 'b' | 'c'] extends ['a']
+ *
+ * @see https://github.com/type-challenges/type-challenges/issues/24885
+ */
+type IsUnion<I, T = I> = isNever<I> extends true ? false : I extends I ? [T] extends [I] ? false : true : never
+
+type test_01097 = IsUnion<string | number>
+
+type test_01097_1 = IsUnion<never>
--- questions/01130-medium-replacekeys/template.ts
+++ questions/01130-medium-replacekeys/template.ts
@@ -1,1 +1,28 @@
-type ReplaceKeys<U, T, Y> = any
+// 替换单对象
+type ReplaceObjKey<O extends Record<string, unknown>, K, V> = {
+  [OK in keyof O]: OK extends K ? V : O[OK]
+}
+
+// type ReplaceKeys<U extends Record<string, unknown>, T, Y> =
+//     U extends U ?
+//     //   T extends keyof U ?
+//       T extends keyof Y ? ReplaceObjKey<U, T, Y[T]> : ReplaceObjKey<U, T, never>
+//     // : U
+//       : never
+
+type Nodes = { name: string; flag: boolean } | {
+  // eslint-disable-next-line @typescript-eslint/member-delimiter-style
+  name: string; no: number
+}
+
+type ReplaceKeys<T extends Record<string, unknown>, U, V> = T extends T ? {
+  [K in keyof T]: K extends U
+    ? K extends keyof V
+      ? V[K]
+      : never
+    : T[K]
+} : never
+
+type test_01130 = ReplaceObjKey<{ a: 1 }, 'a', { a: 2 }>
+
+type test_01130_1 = ReplaceKeys<Nodes, 'name' | 'flag', { name: 1 }>
