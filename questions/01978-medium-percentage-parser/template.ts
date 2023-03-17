0fbae7e4662cad552f37f7c72d104efe9e44dc2d
nzreal <296488372@qq.com>
2023-03-17 11:00:22 +0800

    chore: 01978

--- questions/01978-medium-percentage-parser/template.ts
+++ questions/01978-medium-percentage-parser/template.ts
@@ -1,1 +1,13 @@
-type PercentageParser<A extends string> = any
+type SignParser<T> = T extends `${infer S}${infer R}` ?
+  S extends '+' | '-' ? S : ''
+  : ''
+
+type PercentParser<T> = T extends `${string}%` ? '%' : ''
+
+type NumberParser<T> = T extends `${SignParser<T>}${infer N}${PercentParser<T>}` ? N : ''
+
+type PercentageParser<A extends string> = [SignParser<A>, NumberParser<A>, PercentParser<A>]
+
+function percentageParser(str: string, res: string[] = [], tempNum?: string) {
+
+}
