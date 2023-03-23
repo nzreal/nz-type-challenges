5095fc580479c89c815d26cb90f009b8f48607f8
nzreal <296488372@qq.com>
2023-03-23 15:34:08 +0800

    chore: 02757

--- questions/02757-medium-partialbykeys/template.ts
+++ questions/02757-medium-partialbykeys/template.ts
@@ -1,1 +1,15 @@
-type PartialByKeys<T, K> = any
+type Flatten1<T> = T extends T ? {
+  [K in keyof T]: T[K]
+} : never
+
+type PartialByKeys<T, K extends keyof T = keyof T> = Flatten1<
+    Omit<T, K> & { [FK in K]?: T[FK] }
+>
+
+type test02757 = PartialByKeys<{
+  a: number
+  b: string
+  c: {
+    c1: number
+  }
+}, 'a'>
